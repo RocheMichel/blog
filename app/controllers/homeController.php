80bc57478b18e539df43e3e194f53b67edbbc9bd
RocheMichel <michel.roche@le-campus-numerique.fr>
2023-07-04 09:46:39 +0200

    modification fichier bis4

--- app/controllers/homeController.php
+++ app/controllers/homeController.php
@@ -1,4 +1,9 @@
 <?php
 include 'app/persistances/blogPostData.php';
+include 'ressource/views/home.tpl.php';
 $articlesData = lastBlogPost($pdo);
 var_dump($articlesData);
+
+
+
+
---
+++ ressource/views/home.tpl.php
@@ -0,0 +1,12 @@
+<?php
+include 'ressource/views/layouts/header.tpl.php';
+?>
+<main>
+
+<p> hello word</p>
+
+</main>
+
+<?php
+include 'ressource/views/layouts/footer.tpl.php';
+?>
