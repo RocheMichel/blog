c2f7f720afa63264abc833c4f7fdf634b642a3b5
RocheMichel <michel.roche@le-campus-numerique.fr>
2023-07-06 16:21:07 +0200

    modification des fichiers bis10

--- app/controllers/blogPostCreateController.php
+++ app/controllers/blogPostCreateController.php
@@ -2,8 +2,15 @@
 <?php
 //j'importe les fonctions pour avoir les infos de la base de donnée des articles et commentaires des auteurs
 include 'app/persistances/blogPostData.php';
+
+if(isset($_POST['envoi'])){ // si formulaire soumis
+    $createarticle = blogPostCreate($pdo,$_POST['title'], $_POST['content'],$_POST['raking'],$_POST['publication_date'],$_POST['author_id']);
+}else{
+    echo'Les informations non pas étaient transmis';
+}
+
 //on définie les variables utiles pour le views
-$createarticle = blogPostCreate($pdo,'sport','gfsgf',2,'2023-06-28 00:00:00',4);
+
 
 include 'ressource/views/layouts/header.tpl.php';
 include 'ressource/views/layouts/blogPostCreate.tpl.php';
--- ressource/views/layouts/blogPostCreate.tpl.php
+++ ressource/views/layouts/blogPostCreate.tpl.php
@@ -1,14 +1,48 @@
+<h1>Venais éditer votre article yoouuuhh</h1>
+<form action="?action=blogPostCreate" method="post" class="form-example">
+    <div class="form-example">
+        <label for="title">Titre de l'article </label>
+        <input type="text" name="title" id="title" required>
+    </div><br><br>
+    <label for="content">Editer votre article</label>
 
-<form action="" method="post" class="form-example">
-    <div class="form-example">
-        <label for="title">Enter your name: </label>
-        <input type="text" name="" id="name" required>
+    <textarea id="content" name="content"
+              rows="5" cols="33">
+
+    </textarea><br><br>
+    <label for="publication_date">Date d'édition de l'article</label>
+
+    <input type="datetime-local" id="publication_date"
+           name="publication_date" value="2018-06-12T19:30"
+           min="2018-06-07T00:00" max="2018-06-14T00:00"><br><br>
+
+    <label for="raking">Classement par importance</label><br><br>
+    <input type="number" id="raking" name="raking"
+           min="1" max="10"><br><br>
+
+    <legend>Nom de l'auteur</legend>
+
+    <div>
+        <input type="radio" id="author_id1" name="author_id" value="last_name"
+               checked>
+        <label for="author_id">Michel</label>
     </div>
-    <div class="form-example">
-        <label for="email">Enter your email: </label>
-        <input type="email" name="email" id="email" required>
+    <div>
+        <input type="radio" id="author_id2" name="author_id" value="last_name"
+               checked>
+        <label for="author_id">Mitchi</label>
     </div>
-    <div class="form-example">
-        <input type="submit" value="Subscribe!">
+    <div>
+        <input type="radio" id="author_id" name="author_id" value="last_name"
+               checked>
+        <label for="author_id">Mickelini</label>
     </div>
+
+
+<input type="submit" name="envoi"><br><br>
+<div class="controls">
+
+    <input type="reset" value="Reset">
+
+</div>
 </form>
